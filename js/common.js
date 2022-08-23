2e04fe360ebbf0a8d71f6653b0dd179306383e5b
parkseonghak <100550415+parkseonghak@users.noreply.github.com>
2022-08-23 16:59:51 +0900

    menu_bar_action

--- js/common.js
+++ js/common.js
@@ -3,9 +3,9 @@ const body = document.querySelector('body');
 const hidden = 'hidden'
 
 trgAside.addEventListener('click', () => {
-    if(body.style.overflow === 'visible'){
+    if(body.className !== 'asideOpen'){
         body.classList.add('asideOpen')
-    }else{
+    }else if(body.className == 'asideOpen'){
         body.classList.remove('asideOpen')
     }
 })
